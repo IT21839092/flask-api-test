96f91ce86f769ae202b9c8192d5097234ffcab49
IT21839092 <it21839092@my.sliit.lk>
2024-11-18 12:36:01 +0530

    Changes for deploying

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
@@ -14,5 +15,9 @@ def add():
     num2 = data.get('num2')
     return jsonify({'result': num1 + num2})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
